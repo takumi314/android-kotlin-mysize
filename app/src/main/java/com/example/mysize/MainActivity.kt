3736ed8c19aed7de57cee9c3f13bcdb0835be1db
takumi314 <isospin_symmetry@me.com>
2020-01-15 23:46:03 +0900

    implement save action tapped, input's value

--- app/src/main/java/com/example/mysize/MainActivity.kt
+++ app/src/main/java/com/example/mysize/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.mysize
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 共有プレファレンスから保存されている定数を取得する
+        val pref= PreferenceManager.getDefaultSharedPreferences(this)
+        val editNeck = pref.getString("NECK", "")
+        val editSleeve = pref.getString("SLEEVE", "")
+        val editWaist = pref.getString("WAIST", "")
+        val editInseam = pref.getString("INSEAM", "")
+
+        // 取得した値をEditTextビューに表示する
+        neck.setText(editNeck)
+        sleeve.setText(editSleeve)
+        waist.setText(editWaist)
+        inseam.setText(editInseam)
+
+        // 保存ボタンタップ時の処理を登録する
+        save.setOnClickListener { this.onSaveTapped() }
     }
+
+    private fun onSaveTapped() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.edit {
+            putString("NECK", neck.text.toString())
+            putString("SLEEVE", sleeve.text.toString())
+            putString("WAIST", waist.text.toString())
+            putString("INSEAM", inseam.text.toString())
+        }
+    }
+
 }
